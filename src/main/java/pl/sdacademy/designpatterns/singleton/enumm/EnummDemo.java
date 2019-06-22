cb943976a709f8213ac30b54ef7785ea6ecf6f23
bpiotrowski1 <baej.p26@gmail.com>
2019-06-22 10:18:21 +0200

    added singleton enum

---
+++ src/main/java/pl/sdacademy/designpatterns/singleton/enumm/EnummDemo.java
@@ -0,0 +1,10 @@
+package pl.sdacademy.designpatterns.singleton.enumm;
+
+public class EnummDemo {
+    public static void main(String[] args) {
+        final UsersRepository usersRepositoryA = UsersRepository.INSTANCE;
+        final UsersRepository usersRepositoryB = UsersRepository.INSTANCE;
+
+
+    }
+}
---
+++ src/main/java/pl/sdacademy/designpatterns/singleton/enumm/UsersRepository.java
@@ -0,0 +1,8 @@
+package pl.sdacademy.designpatterns.singleton.enumm;
+
+public enum UsersRepository {
+    // prywatny konstruktor
+    // getInstance
+    // pole statyczne klasy
+    INSTANCE;
+}
